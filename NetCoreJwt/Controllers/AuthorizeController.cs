c39605148ce1512fc5d507fd111d702158342f84
huangjc <huangjc@HUANGJC>
2019-05-08 21:08:03 +0800

    添加项目文件。

---
+++ NetCoreJwt/Controllers/AuthorizeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NetCoreJwt.Models;
+
+namespace NetCoreJwt.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class AuthorizeController : Controller
+    {
+         private JwtSettings setting;
+         public AuthorizeController(IOptions<JwtSettings> options)
+         {
+             setting = options.Value;
+         }
+ 
+         [HttpPost]
+         public IActionResult Login(LoginViewModel login)
+         {
+             if (ModelState.IsValid)
+             {
+                 if (login.UserName == "wangshibang" && login.Password == "123456")
+                 {
+                     var claims = new Claim[] {
+                         new Claim(ClaimTypes.Name, login.UserName),
+                         new Claim(ClaimTypes.Role, "admin, Manage")
+                     };
+                     var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(setting.SecretKey));
+                     var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                     var token = new JwtSecurityToken(
+                         setting.Issuer,
+                         setting.Audience,
+                         claims,
+                         DateTime.Now,
+                         DateTime.Now.AddMinutes(30),
+                         creds);
+                     return Ok(new { Token = new JwtSecurityTokenHandler().WriteToken(token) });
+                 }
+             }
+             return BadRequest();
+         }
+ 
+         [HttpGet]
+         public IActionResult NoValidate()
+         {
+             return Ok();
+         }
+     
+    }
+}
---
+++ NetCoreJwt/Startup.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NetCoreJwt.Models;
+
+namespace NetCoreJwt
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<JwtSettings>(Configuration.GetSection("JwtSettings"));
+            JwtSettings setting = new JwtSettings();
+            Configuration.Bind("JwtSettings", setting);
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(config =>
+            {
+                /**/
+                config.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidAudience = setting.Audience,
+                    ValidIssuer = setting.Issuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(setting.SecretKey))
+                };
+                
+                /*
+                config.SecurityTokenValidators.Clear();
+                config.SecurityTokenValidators.Add(new MyTokenValidate());
+                config.Events = new JwtBearerEvents()
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var token = context.Request.Headers["myToken"];
+                        context.Token = token.FirstOrDefault();
+                        return Task.CompletedTask;
+                    }
+
+                };
+                */
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
